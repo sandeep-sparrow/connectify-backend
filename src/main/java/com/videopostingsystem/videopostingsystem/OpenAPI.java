19135711e3cc5b977d5916812ddc18e0f9ca292c
Noah Solomon <noahsolomon2003@gmail.com>
2023-04-14 15:57:57 -0400

    Separated rest controllers from business logic and also created logic that deletes post interactions from the post interaction table whenever a post is deleted. Soon this implementation will be used to delete post interactions from the table as well as posts from its table when they associate with a user who deletes their account

--- src/main/java/com/videopostingsystem/videopostingsystem/OpenAPI.java
+++ src/main/java/com/videopostingsystem/videopostingsystem/OpenAPI.java
@@ -7,7 +7,6 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Scanner;
 
 public class OpenAPI {
 
