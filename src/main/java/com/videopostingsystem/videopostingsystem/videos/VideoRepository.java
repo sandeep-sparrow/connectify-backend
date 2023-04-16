5a979c8abb69fb8eee3c8bb331185d50ab0cb77c
Noah Solomon <noahsolomon2003@gmail.com>
2023-04-16 10:14:35 -0400

    deleted video feature for now. It might come back if it does, it will be after the frontend work is complete.

--- src/main/java/com/videopostingsystem/videopostingsystem/videos/VideoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.videopostingsystem.videopostingsystem.videos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VideoRepository extends JpaRepository<Video, Long> {
-}
