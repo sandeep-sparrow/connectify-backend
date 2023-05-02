a1287e9b7c6c62f9aaa490210c5adb4b9f293611
noahsolomon <noahsolomon2003@gmail.com>
2023-05-02 17:43:01 -0400

    Fixed for deployment

---
+++ src/main/java/com/videopostingsystem/videopostingsystem/openapi/CONSTANTS.java
@@ -0,0 +1,5 @@
+package com.videopostingsystem.videopostingsystem.openapi;
+
+public class CONSTANTS {
+    public static String OPEN_API_KEY = System.getenv("OPEN_API_KEY");
+}
---
+++ src/main/java/com/videopostingsystem/videopostingsystem/users/CONSTANTS.java
@@ -0,0 +1,5 @@
+package com.videopostingsystem.videopostingsystem.users;
+
+public class CONSTANTS {
+    public static String security_clearance = System.getenv("SECURITY_CLEARANCE");
+}
