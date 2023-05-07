39f79d5a1c903e2d16c793ba0c5575520bf19ecd
Noah Solomon <noahsolomon2003@gmail.com>
2023-05-07 01:52:57 -0400

    added follow table and repository.

--- src/main/java/com/videopostingsystem/videopostingsystem/users/follow/Follow.java
+++ src/main/java/com/videopostingsystem/videopostingsystem/users/follow/Follow.java
@@ -1,4 +1,33 @@
 package com.videopostingsystem.videopostingsystem.users.follow;
 
+import com.videopostingsystem.videopostingsystem.users.Users;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"follower", "following"})})
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "follower", nullable = false)
+    private Users follower;
+
+    @ManyToOne
+    @JoinColumn(name = "following", nullable = false)
+    private Users following;
+
+    public Follow(Users follower, Users following){
+        this.follower = follower;
+        this.following = following;
+    }
+
 }
---
+++ src/main/java/com/videopostingsystem/videopostingsystem/users/follow/FollowRepository.java
@@ -0,0 +1,16 @@
+package com.videopostingsystem.videopostingsystem.users.follow;
+
+import com.videopostingsystem.videopostingsystem.users.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    //to retrieve users following list
+    List<Follow> findAllByFollower(Users follower);
+
+    //to retrieve users followers list
+    List<Follow> findAllByFollowing(Users following);
+
+}
